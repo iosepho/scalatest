2e072e57eb3c37f2d2c037e67c040ad048c3e3af
Ioseph O Loingsigh <ioloingsigh@spotxchange.com>
2015-03-03 11:28:11 +0000

    Auto build test 2

--- src/test/scala/EmailTest.scala
+++ src/test/scala/EmailTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class EmailTest extends FunSuite{
     test("Email with valid address")
     {
-        val email = Email("test@valid.com")
+        val email = Email("test2@valid.com")
         assert(email.address != null)
     }
 
